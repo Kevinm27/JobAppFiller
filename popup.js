7ca0874e1d76cb5360ac45488cf96d4d49466085
Kevin <kevinmaldonadocota@gmail.com>
2023-08-07 02:19:28 -0700

    Popup now has two windows, one for saved information and another for inputting information. Data is now saved into google storage. Added basic CSS for the html

--- popup.js
+++ popup.js
@@ -2,7 +2,12 @@ document.getElementById('saveData').addEventListener('click', function() {
     const data = {
         firstName: document.getElementById('firstName').value,
         lastName: document.getElementById('lastName').value,
-        // ... collect data from other fields similarly ...
+        address: document.getElementById('address').value,
+        city: document.getElementById('city').value,
+        state: document.getElementById('state').value,
+        postalCode: document.getElementById('postalCode').value,
+        email: document.getElementById('email').value,
+        phoneNumber: document.getElementById('phoneNumber').value
     };
     chrome.storage.sync.set({userInfo: data}, function() {
         console.log('User information saved.');
@@ -16,14 +21,44 @@ document.getElementById('fillForm').addEventListener('click', function() {
     });
 });
 
-
 document.addEventListener('DOMContentLoaded', function() {
     chrome.storage.sync.get('userInfo', function(data) {
         if (data.userInfo) {
+            //data from inputted fields
             document.getElementById('firstName').value = data.userInfo.firstName || '';
             document.getElementById('lastName').value = data.userInfo.lastName || '';
-            // ... Load other fields here ...
+            document.getElementById('address').value = data.userInfo.address || '';
+            document.getElementById('city').value = data.userInfo.city || '';
+            document.getElementById('state').value = data.userInfo.state || '';
+            document.getElementById('postalCode').value = data.userInfo.postalCode || '';
+            document.getElementById('email').value = data.userInfo.email || '';
+            document.getElementById('phoneNumber').value = data.userInfo.phoneNumber || '';
+            
+            //saved information below
+            document.getElementById('savedFirstName').textContent = data.userInfo.firstName || 'Not Saved';
+      	    document.getElementById('savedLastName').textContent = data.userInfo.lastName || 'Not Saved';
+      	    document.getElementById('savedAddress').textContent = data.userInfo.address || 'Not Saved';
+      	    document.getElementById('savedCity').textContent = data.userInfo.city || 'Not Saved';
+      	    document.getElementById('savedState').textContent = data.userInfo.state || 'Not Saved';
+      	    document.getElementById('savedPostalCode').textContent = data.userInfo.postalCode || 'Not Saved';
+      	    document.getElementById('savedEmail').textContent = data.userInfo.email || 'Not Saved';
+     	    document.getElementById('savedPhoneNumber').textContent = data.userInfo.phoneNumber || 'Not Saved';
         }
     });
+});
+
+document.getElementById('viewData').addEventListener('click', function() {
+    document.getElementById('newDataSection').style.display = 'none';
+    document.getElementById('savedDataSection').style.display = 'block';
+});
+
+document.getElementById('enterData').addEventListener('click', function() {
+    document.getElementById('savedDataSection').style.display = 'none';
+    document.getElementById('newDataSection').style.display = 'block';
+});
+
+document.getElementById('editData').addEventListener('click', function() {
+    document.getElementById('savedDataSection').style.display = 'none';
+    document.getElementById('newDataSection').style.display = 'block';
 });
 
